f70f258c35301581eeef781c442bbc732faff1ba
wrzasa <wrzasa@rzeszow.oaza.pl>
2017-02-22 12:06:48 +0100

    Some stats computing examples in Experiment class

--- lib/rbsim_example/experiment.rb
+++ lib/rbsim_example/experiment.rb
@@ -1,3 +1,18 @@
 class Experiment < RBSim::Experiment
-  # put here methods that process staistics from your experiment
+
+  def print_req_times_for(server)
+    app_stats.durations(server: server) do |tags, start, stop| 
+      puts "Request time #{(stop - start).in_miliseconds} ms. "
+    end
+  end
+
+  def mean_req_time_for(server)
+    req_times = app_stats.durations(server: server).to_a
+    sum = req_times.reduce(0) do |acc, data|
+      _, start, stop = *data
+      acc + stop - start
+    end
+    sum / req_times.size
+  end
+
 end
